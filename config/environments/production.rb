e89a1e41d63456f59cf56cb537b9ffb7ea223c32
Nikhildanand <nikhildanand@gmail.com>
2017-12-11 00:30:12 +0530

    bg fa error heroku try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,9 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
+  config.assets.precompile += %w( 
+    *.svg *.eot *.woff *.ttf *.png *.jpg *.jpeg *.gif
+  )
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
